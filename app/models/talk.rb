f462fcc24b1c999964412f19b64fcfcf6625eb6b
coreyhaines <coreyhaines@gmail.com>
2012-07-29 09:56:57 -0500

    Add scope Talk.sorted_by_vote_count

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,6 +1,8 @@
 class Talk < ActiveRecord::Base
   attr_accessible :title, :vote_count
 
+  scope :sorted_by_vote_count, order("vote_count DESC")
+
   def add_vote!
     self.update_attribute :vote_count, self.vote_count+1
   end
