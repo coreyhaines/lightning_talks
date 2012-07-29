ba71083fbf66c9801f8b17841d8a686d4ed94f71
coreyhaines <coreyhaines@gmail.com>
2012-07-29 09:30:46 -0500

    Add vote count to seed file

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,3 +1,3 @@
 class Talk < ActiveRecord::Base
-  attr_accessible :title
+  attr_accessible :title, :vote_count
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+Talk.destroy_all
 10.times do |index|
-  Talk.create title: "Talk Number #{index}"
+  Talk.create title: "Talk Number #{index}", vote_count: rand(10)
 end
