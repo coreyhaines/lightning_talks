2a419a97d34f52c310b6a398840246f49d42d1ef
ywong <ywong@ywong-laptop.cashnetusa.com>
2012-07-31 14:21:08 -0500

    Minor changes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,7 @@
 class VotesController < ApplicationController
   def create
-    @book = Book.find(params[:book])
-    @book.add_vote!
+    book = Book.find(params[:book_id])
+    book.add_vote!
     redirect_to books_url    
   end
 end
